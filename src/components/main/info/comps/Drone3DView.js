13eef26dfb419d20f7199a10ec4b362888f37fa6
Hyunnn2 <gusrud5015@gmail.com>
2024-05-03 13:47:53 +0900

    3D 애니메이션 수정

--- src/components/main/info/comps/Drone3DView.js
+++ src/components/main/info/comps/Drone3DView.js
@@ -1,33 +1,70 @@
 import React, { useEffect, useRef } from 'react';
 import './Drone3DView.css';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
+
 
 const Drone3DView = () => {
     const containerRef = useRef(null);
+    const rendererRef = useRef(null);
+    const cameraRef = useRef(null);
 
     useEffect(() => {
         const container = containerRef.current;
 
-        // Scene
-        const scene = new THREE.Scene();
+        // 창 크기 변경 시 적절한 조치를 취하기 위한 핸들러
+        const handleResize = () => {
+            const renderer = rendererRef.current;
+            const camera = cameraRef.current;
+            if (renderer && camera ) {
+                const width = container.offsetWidth;
+                const height = container.offsetHeight;
+                renderer.setSize(width, height);
+                camera.aspect = width / height;
+                camera.updateProjectionMatrix();
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
         
-        // Camera
-        const camera = new THREE.PerspectiveCamera(50, container.offsetWidth / container.offsetHeight, 0.1, 1000);
-        camera.position.set(0, 0, 0.25); // 카메라의 위치 설정
 
-        // Renderer
-        const renderer = new THREE.WebGLRenderer({ antialias: true }); // 안티앨리어싱 활성화
+        // Three.js Scene 생성
+        const scene = new THREE.Scene();
+
+        // Three.js Camera 생성
+        const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000); // 초기 비율을 1로 설정
+        camera.position.set(0, 0.5, 0);
+        camera.lookAt(0, 0, 0);
+
+        // Three.js WebGLRenderer 생성
+        const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
         renderer.setSize(container.offsetWidth, container.offsetHeight);
-        renderer.setClearColor(0xfffff0);
         container.appendChild(renderer.domElement);
+        renderer.setClearAlpha(1);
+        renderer.setClearColor(0xD1EEFC, 1);
 
-        // Add directional light
+        // Three.js WebGLRenderer 배경 설정
+        renderer.setClearColor(0x000000, 0); // 배경색을 투명으로 설정
+        const canvas = renderer.domElement;
+        //canvas.style.background = 'radial-gradient(circle farthest-corner at center top, #071021, #19324a)';
+        canvas.style.background = 'radial-gradient(circle farthest-corner at center top, #505C81, #56728C)';
+        //canvas.style.background = 'radial-gradient(circle farthest-corner at center top, #294361, #505C81)';
+
+         // CSS2DRenderer 생성
+         const cssRenderer = new CSS2DRenderer();
+         cssRenderer.setSize(container.offsetWidth, container.offsetHeight);
+         cssRenderer.domElement.style.position = 'absolute';
+         cssRenderer.domElement.style.top = 0;
+         cssRenderer.domElement.style.left = 0;
+         container.appendChild(cssRenderer.domElement);
+
+        // Directional Light 추가
         const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
-        directionalLight.position.set(1, 1, 1).normalize(); // 조명의 위치 설정
+        directionalLight.position.set(1, 1, 1).normalize();
         scene.add(directionalLight);
 
-         // Load Drone GLTF model
+        // Drone 모델 로드
         const loader = new GLTFLoader();
         loader.load(
             'animated_drone/scene.gltf', 
@@ -35,14 +72,16 @@ const Drone3DView = () => {
                 const drone = gltf.scene;
                 scene.add(drone);
                 console.log('Model loaded successfully:', drone);
-
-                drone.scale.set(0.8, 0.8, 0.8);
-                
-                // Animation
+                drone.scale.set(1, 1, 1);
+            
                 const animate = () => {
                     requestAnimationFrame(animate);
-                    drone.rotation.y += 0.01; // 회전 애니메이션
+                
+                    drone.rotation.x += 0.01; //pitch
+                    drone.rotation.y += 0.01; //yaw
+                    drone.rotation.z += 0.01; //roll
                     renderer.render(scene, camera);
+                    cssRenderer.render(scene, camera);
                 };
 
                 animate();
@@ -53,14 +92,38 @@ const Drone3DView = () => {
             }
         );
 
+
+        // Compass 추가
+        const compassDiv = document.createElement('div');
+        compassDiv.className = 'compass';
+        compassDiv.innerHTML = `
+            <div class="directionNorth">N</div>
+            <div class="directionSouth">S</div>
+            <div class="directionEast">E</div>
+            <div class="directionWest">W</div>
+        `;
+        const compassObject = new CSS2DObject(compassDiv);
+        scene.add(compassObject);
+
+        // 렌더러 및 카메라 참조 저장
+        rendererRef.current = renderer;
+        cameraRef.current = camera;
+
+        handleResize(); // 초기 렌더러 및 카메라 크기 설정
+
+       
+
+        // 클린업 함수
         return () => {
+            window.removeEventListener('resize', handleResize);
             container.removeChild(renderer.domElement);
+            container.removeChild(cssRenderer.domElement);
         };
     }, []);
 
     return (
         <div className='Drone3DViewContainer' ref={containerRef}>
-            <h2>Drone 3D view</h2>
+        
         </div>
     );
 };
